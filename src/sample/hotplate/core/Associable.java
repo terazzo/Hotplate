44d91b0692aff3fcfabadfe695610f7d44a18d49
terazzo <terazzo@mbp.ocn.ne.jp>
2011-03-12 21:08:28 +0900

    消し忘れ

--- src/sample/hotplate/core/Associable.java
+++
@@ -1,7 +0,0 @@
-package sample.hotplate.core;
-
-public interface Associable<V, T extends Template<V, T>> {
-	boolean isTemplate();
-	Value<V, T> asValue();
-	Template<V, T> asTemplate();
-}
--- src/sample/hotplate/core/Value.java
+++
@@ -1,5 +0,0 @@
-package sample.hotplate.core;
-
-public interface Value<V, T extends Template<V, T>> extends Associable<V, T> {
-	V value();
-}

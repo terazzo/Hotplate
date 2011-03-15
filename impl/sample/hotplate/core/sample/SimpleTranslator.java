8768beb6185381fa54a889fd0c166981c996bd74
terazzo <terazzo@mbp.ocn.ne.jp>
2011-03-15 18:38:48 +0900

    シンプル版JParsecバージョン

--- impl/sample/hotplate/core/sample/SimpleTranslator.java
+++ impl/sample/hotplate/core/sample/SimpleTranslator.java
@@ -1,8 +1,12 @@
 package sample.hotplate.core.sample;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
+
+import org.codehaus.jparsec.Parser;
+import org.codehaus.jparsec.Parsers;
+import org.codehaus.jparsec.Scanners;
+import org.codehaus.jparsec.functors.Map;
+import org.codehaus.jparsec.pattern.Patterns;
 
 import sample.hotplate.core.Symbol;
 import sample.hotplate.core.Translator;
@@ -19,37 +23,29 @@ public class SimpleTranslator implements Translator<String, Object, SimpleTempla
 	public String fromTemplate(SimpleTemplate template) {
 		return template.getString();
 	}
-	enum Mode {
-		TEXT,
-		IN_BRACE
-	}
-	private List<SimpleTemplate> parse(String rawObject) {
-		List<SimpleTemplate> elements = new ArrayList<SimpleTemplate>();
-		StringTokenizer stringTokenizer = new StringTokenizer(rawObject, "{}", true);
-		Mode mode = Mode.TEXT;
-		while (stringTokenizer.hasMoreTokens()) {
-			String token = stringTokenizer.nextToken();
-			switch (mode) {
-			case TEXT:
-				if (token.equals("{")) {
-					mode = Mode.IN_BRACE;
-				} else if (token.equals("}")) {
-					throw new IllegalArgumentException("Illegal format string. Unexpected '}' occured." + rawObject);
-				} else {
-					elements.add(new SimpleLiteral(token));
-				}
-				break;
-			case IN_BRACE:
-				if (token.equals("{")) {
-					throw new IllegalArgumentException("Illegal format string. Unexpected '{' occured." + rawObject);
-				} else if (token.equals("}")) {
-					mode = Mode.TEXT;
-				} else {
-					elements.add(new SimpleProcessor(Symbol.of(token)));				
-				}
-				break;
-			}
-		}
-		return elements;
-	}
+
+	Parser<List<SimpleTemplate>> templateParser = createParser();
+    private Parser<List<SimpleTemplate>> createParser() {
+
+        Parser<String> braceStart = Scanners.isChar('{').source();
+        Parser<String> braceEnd = Scanners.isChar('}').source();
+        Parser<String> textParser = 
+            Scanners.pattern(Patterns.regex("((\\\\\\{)|(\\\\\\})|[^{}])*") ,"TEXT").source();
+
+        
+        Parser<SimpleTemplate> placeHolder =  textParser.between(braceStart, braceEnd).map(new Map<String, SimpleTemplate>() {
+             public SimpleTemplate map(String text) {
+                 return new SimpleProcessor(Symbol.of(text));
+            }
+        });
+        Parser<SimpleTemplate> literal = textParser.map(new Map<String, SimpleTemplate>() {
+            public SimpleTemplate map(String text) {
+                return new SimpleLiteral(text);
+           }
+        });
+        return Parsers.or(placeHolder, literal).many();
+    }
+    private List<SimpleTemplate> parse(String rawObject) {
+        return templateParser.parse(rawObject);
+    }
 }
--- src/sample/hotplate/core/Symbol.java
+++ src/sample/hotplate/core/Symbol.java
@@ -24,7 +24,7 @@ public final class Symbol {
 	}
 	@Override
 	public String toString() {
-		return String.format("<%s: %s>", getClass(), label);
+		return String.format("<%s: '%s'>", getClass(), label);
 	}
 	
 }
